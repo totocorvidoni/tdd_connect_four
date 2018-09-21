736f06210ddebc1190f7a46d0e1f26976c0836e2
Toto Corvidoni <TotoCorvidoni@gmail.com>
2018-09-21 14:48:00 -0300

    Add Player class and RSpec examples for it

---
+++ spec/player_spec.rb
@@ -0,0 +1,30 @@
+require 'player'
+
+describe Player do
+  subject(:player) { Player.new('Pupe', "\u26aa") }
+
+  it { is_expected.to respond_to(:name) }
+
+  describe '#name shows player name' do
+    context 'Player name is Pupe' do
+      it "returns 'Pupe'" do
+        expect(player.name).to eq('Pupe')
+      end
+    end
+  end
+
+  describe '#mark represents the player piece ' do
+    context 'when mark is an empty circle' do
+      it "returns \u26aa" do
+        expect(player.mark).to eq("\u26aa")
+      end
+    end
+
+    context 'when mark is a circle' do
+      let(:circle) { Player.new('circle', "\u26aa") }
+      it "returns \u26ab" do
+        expect(circle.mark).to eq("\u26aa")
+      end
+    end
+  end
+end
